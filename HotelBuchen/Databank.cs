2389266ae07c0f2a39e92a08d187c533b75ee2c4
Vincent Veeh <vincentveeh@gmail.com>
2022-10-31 11:05:12 +0100

    a

---
+++ HotelBuchen/Databank.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBuchen
+{
+    internal class Databank
+    {
+        string workingDirectory = Environment.CurrentDirectory;
+        string dir;
+        OleDbConnection con;
+        public OleDbCommand Command;
+        public Databank()
+        {
+            dir = Directory.GetParent(workingDirectory).Parent.FullName;
+            con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + dir + "\\Ferienwohnungen.mdb");
+        }
+    }
+}
